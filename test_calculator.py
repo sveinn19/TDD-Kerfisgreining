7bebf29443f827fed081c4c10453c6ebafdad553
unknown <sveinni19@ru.is>
2020-03-27 01:19:24 +0000

    RED negative nums

--- test_calculator.py
+++ test_calculator.py
@@ -21,3 +21,6 @@ def test_add_with_newline_delimater():
 def test_add_num_bigger_1000():
     assert Calculator.add("1002,2") == 2
 
+def test_negative_nums():
+    assert Calculator.add("-4,2") == "Negatives are not allowed: -4"
+
